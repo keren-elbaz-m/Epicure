dc0974fe26ae45bb707c34fbafb9159daab3a782
keren-elbaz-m <kerene@moveo.co.il>
2025-04-29 15:31:49 +0300

    get request done

--- src/api/dish/controllers/custom-dish.ts
+++ src/api/dish/controllers/custom-dish.ts
@@ -6,7 +6,19 @@ export default {
             const {id} = ctx.params;
             const dish = await strapi.db.query("api::dish.dish").findOne({
                 where: {id},
-                populate: '*'
+                select: ["id","name", "price", "ingredients"],
+                populate: {
+                    image: {select: ["name", "url", "width", "height", "alternativeText"]},
+                    dish_type: {
+                        select: ["name"],
+                        populate: {
+                            icon: { select: ["url", "width", "height", "alternativeText"] },
+                        },
+                    },
+                    side_dish: {select: ["name"]},
+                    dish_changes: {select: ["name"]},
+                    meal_time: {select: ["name"]},
+                }
             });
             return ctx.body = dish;
         } catch (error) {
@@ -19,24 +31,26 @@ export default {
         try {
             const { query } = ctx;
             const dishes = await strapi.entityService.findMany("api::dish.dish", {
-              fields: ["id", "name", "price", "ingredients"],
-              populate: {
-                image: { fields: ["url", "width", "height", "alternativeText"] },
-                dish_tags: {
-                  fields: ["name"],
-                  populate: {
-                    icon: { fields: ["url", "width", "height", "alternativeText"] },
+                fields: ["id", "name", "price", "ingredients"],
+                populate: {
+                  image: { fields: ["url", "width", "height", "alternativeText"] },
+                  dish_type: {
+                    fields: ["name"],
+                    populate: {
+                      icon: { fields: ["url", "width", "height", "alternativeText"] },
+                    },
                   },
+                  side_dish: { fields: ["name"] },
+                  dish_changes: { fields: ["name"] },
                 },
-                side_dishes: { fields: ["name"] },
-                dish_changes: { fields: ["name"] },
-              },
-              ...query,
-            });
+                ...query,
+            }) 
+              
             return (ctx.body = dishes);
           } catch (error) {
             ctx.status = 500;
             return (ctx.body = { error: error.message });
           }
-    }
+    } 
+
 }
