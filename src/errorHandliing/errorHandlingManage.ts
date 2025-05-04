8ebb0f00f1367f498b91049f0d0d887f6501aecc
keren-elbaz-m <kerene@moveo.co.il>
2025-05-04 12:50:51 +0300

    Update errorHandlingManage.ts

--- src/errorHandliing/errorHandlingManage.ts
+++ src/errorHandliing/errorHandlingManage.ts
@@ -2,24 +2,33 @@ import { ErrorCode } from "../types/errorTypes";
 
 export interface IErrorResponse{
     statusCodeNumber: ErrorCode;
-    devMessage: string;
+    devMessage?: string;
     clientMessage?: string;
+    error?: Error;
 }
 
-export function handleError( statusCodeNumber: ErrorCode, error?: unknown, clientMessage?:string): IErrorResponse{ 
-    let devMessage = "An unknown error occurred.";
-
+export function handleError({ statusCodeNumber, devMessage, clientMessage, error }: {
+  statusCodeNumber: ErrorCode;
+  devMessage?: string;
+  clientMessage?: string;
+  error?: unknown;
+}): IErrorResponse {
+ 
+  if (!devMessage) {
     if (error instanceof Error) {
         devMessage = error.message;
     }else if(typeof error === 'string'){
         devMessage = error;
+    }else{
+        devMessage = "An unknown error occurred.";
     }
+  }
 
-    return {
-        statusCodeNumber,
-        devMessage,
-        clientMessage,
-    };
+  return {
+    statusCodeNumber,
+    devMessage,
+    clientMessage,
+  };
 }
 
 
