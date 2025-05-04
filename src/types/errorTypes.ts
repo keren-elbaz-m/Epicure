951a6a8f9dd9a73544b21dc22047bfb1d0bf0cae
keren-elbaz-m <kerene@moveo.co.il>
2025-05-04 10:56:51 +0300

    Update errorTypes.ts

--- src/types/errorTypes.ts
+++ src/types/errorTypes.ts
@@ -1,36 +1,36 @@
 export type ErrorCode = 400 | 401 | 403 | 404 | 422|  500;
 
-export type IErrorMessage = {
-    [key in ErrorCode]:{
-        devMessage: string;
-        clientMessage: string;
-    }
-}
+// export type IErrorMessage = {
+//     [key in ErrorCode]:{
+//         devMessage: string;
+//         clientMessage: string;
+//     }
+// }
 
-export const errorMessages: IErrorMessage = {
-    400: {
-        devMessage: "Bad Request: Invalid data or missing fields.",
-        clientMessage: "Your request was invalid. Please check and try again.",
-    },
-    401: {
-        devMessage: "Unauthorized: Token or credentials are missing/invalid.",
-        clientMessage: "You are not logged in. Please authenticate.",
-    },
-    403: {
-        devMessage: "Forbidden: User does not have access.",
-        clientMessage: "You are not allowed to do that.",
-    },
-    404: {
-        devMessage: "Not Found: Resource does not exist.",
-        clientMessage: "We couldn't find what you were looking for.",
-    },
-    422: {
-        devMessage: "Unprocessable Entity: Invalid data or missing fields.",
-        clientMessage: "Your request was invalid. Please check and try again.",
-    },
-    500: {
-        devMessage: "Internal Server Error: Something went wrong.",
-        clientMessage: "Oops! Something went wrong. Please try later.",
-    },
+// export const errorMessages: IErrorMessage = {
+//     400: {
+//         devMessage: "Bad Request: Invalid data or missing fields.",
+//         clientMessage: "Your request was invalid. Please check and try again.",
+//     },
+//     401: {
+//         devMessage: "Unauthorized: Token or credentials are missing/invalid.",
+//         clientMessage: "You are not logged in. Please authenticate.",
+//     },
+//     403: {
+//         devMessage: "Forbidden: User does not have access.",
+//         clientMessage: "You are not allowed to do that.",
+//     },
+//     404: {
+//         devMessage: "Not Found: Resource does not exist.",
+//         clientMessage: "We couldn't find what you were looking for.",
+//     },
+//     422: {
+//         devMessage: "Unprocessable Entity: Invalid data or missing fields.",
+//         clientMessage: "Your request was invalid. Please check and try again.",
+//     },
+//     500: {
+//         devMessage: "Internal Server Error: Something went wrong.",
+//         clientMessage: "Oops! Something went wrong. Please try later.",
+//     },
         
-}
+// }
